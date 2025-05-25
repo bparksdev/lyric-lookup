ac0928eead533b727c2600b8d0e31e6769c8bdd4
Brian R. Parks <skrapco@gmail.com>
2025-05-25 15:38:44 -0400

    Tweaks

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/search", async (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
